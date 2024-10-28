53714397d254fa2081c0b8e9282c9c1eb44c580c
SaidSafwan <saidsafwan59@gmail.com>
2024-10-28 22:48:33 +0530

    Add File

---
+++ Interview-Coding-Questions/04.FrequencyCount.py
@@ -0,0 +1,16 @@
+# Question 5: Write a Python program to count the frequency of each element in a list.
+
+num = [1, 2, 3, 1, 3, 4, 2, 2, 3, 5]
+
+def countFrequency(numbers):
+    freq = {} #empty dictionary
+    for num in numbers:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return freq
+
+freq_count = countFrequency(num)
+print(freq_count)
+
